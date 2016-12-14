df8980128187a64917ab9d83ebfddb5262d51205
Wileyan2016 <aneaswiley@yahoo.com>
2016-12-14 07:26:18 -0500

    Initial commit

---
+++ app/src/main/java/com/example/aneas/testpreferencesapp/EarthQuakeActivity.java
@@ -0,0 +1,43 @@
+package com.example.aneas.testpreferencesapp;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class EarthQuakeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_earth_quake);
+
+        SharedPreferences mySharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String minmax = mySharedPreferences.getString("list_prefkey", "dollar");
+        Toast.makeText(this, minmax, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_setttings) {
+            Intent setttingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(setttingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
